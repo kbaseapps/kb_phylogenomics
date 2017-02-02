379f777fde52b637a9beab1f8d9b5194c4175a1c
Dylan Chivian <dcchivian@lbl.gov>
2017-02-02 14:45:24 -0800

    move xvfb-run into subprocess

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -14,7 +14,7 @@ elif [ "${1}" = "test" ] ; then
   echo "Run Tests"
   make test
 elif [ "${1}" = "async" ] ; then
-  xvfb-run sh ./scripts/run_async.sh
+  sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
 elif [ "${1}" = "bash" ] ; then
