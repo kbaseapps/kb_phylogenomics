75c5b83270c6730a7118651a959b04b5c7732617
Dylan Chivian <dcchivian@lbl.gov>
2017-03-21 11:06:36 -0700

    playing with scale

--- lib/kb_phylogenomics/kb_phylogenomicsImpl.py
+++ lib/kb_phylogenomics/kb_phylogenomicsImpl.py
@@ -2890,7 +2890,7 @@ This module contains methods for running and visualizing results of phylogenomic
         ellipse_center_x = 0.50
         ellipse_center_y = 0.50
         ellipse_center = (ellipse_center_x, ellipse_center_y)
-        base_diameter = 0.15
+        base_diameter = 0.20
         gene_bar_lw = genome_ring_scale_factor * 20
         #genome_ring_spacing = 0.2*gene_bar_lw / float(genome_ring_scale_factor)
         genome_ring_spacing = 0.1 * gene_bar_lw
@@ -2948,7 +2948,7 @@ This module contains methods for running and visualizing results of phylogenomic
                         continue
                     gene_color = 'green'
                     #gene_bar_diameter = base_diameter + (genome_i+1)*(gene_bar_lw+genome_ring_spacing)
-                    gene_bar_diameter = base_diameter + (genome_i+1)*(gene_bar_lw+genome_ring_spacing)*lw_to_coord_scale
+                    gene_bar_diameter = base_diameter + (genome_i+1)*(gene_bar_lw)*lw_to_coord_scale
                     gene_x_diameter = 1.0 * gene_bar_diameter
                     gene_y_diameter = ellipse_to_circle_scaling * gene_bar_diameter
                     gene_arc = Arc (ellipse_center, gene_x_diameter, gene_y_diameter, \
