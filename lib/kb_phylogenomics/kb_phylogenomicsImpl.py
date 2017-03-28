b8bb4892a845533402d4b17a7e65dea1de760343
Dylan Chivian <dcchivian@lbl.gov>
2017-03-27 23:45:19 -0700

    added outgroup param to pan_circle_plot()

--- lib/kb_phylogenomics/kb_phylogenomicsImpl.py
+++ lib/kb_phylogenomics/kb_phylogenomicsImpl.py
@@ -51,8 +51,8 @@ This module contains methods for running and visualizing results of phylogenomic
     # the latter method is running.
     ######################################### noqa
     VERSION = "0.0.2"
-    GIT_URL = "https://github.com/rsutormin/kb_phylogenomics"
-    GIT_COMMIT_HASH = "063ee6908b7568290b8c4af98ba69cb7264112dc"
+    GIT_URL = "https://github.com/kbaseapps/kb_phylogenomics.git"
+    GIT_COMMIT_HASH = "960393da7aae57c7e94283b9554bfd4e60d4614c"
 
     #BEGIN_CLASS_HEADER
 
