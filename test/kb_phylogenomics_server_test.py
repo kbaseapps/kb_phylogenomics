b15058e6969ddb8d0727e477a7df44e085928d73
dcchivian <dcchivian@lbl.gov>
2017-09-06 22:13:16 -0700

    progress

--- test/kb_phylogenomics_server_test.py
+++ test/kb_phylogenomics_server_test.py
@@ -3,6 +3,7 @@ import unittest
 import os  # noqa: F401
 import json  # noqa: F401
 import time
+import shutil
 import requests
 
 from os import environ
@@ -13,7 +14,9 @@ except:
 
 from pprint import pprint  # noqa: F401
 
-from biokbase.workspace.client import Workspace as workspaceService
+#from biokbase.workspace.client import Workspace as workspaceService
+from Workspace.WorkspaceClient import Workspace as workspaceService
+from GenomeFileUtil.GenomeFileUtilClient import GenomeFileUtil
 from kb_phylogenomics.kb_phylogenomicsImpl import kb_phylogenomics
 from kb_phylogenomics.kb_phylogenomicsServer import MethodContext
 from kb_phylogenomics.authclient import KBaseAuth as _KBaseAuth
@@ -73,28 +76,112 @@ class kb_phylogenomicsTest(unittest.TestCase):
     def getContext(self):
         return self.__class__.ctx
 
+    # call this method to get the WS object info of a Genome
+    #   (will upload the example data if this is the first time the method is called during tests)
+    def getGenomeInfo(self, genome_basename, lib_i=0):
+        if hasattr(self.__class__, 'genomeInfo_list'):
+            try:
+                info = self.__class__.genomeInfo_list[lib_i]
+                name = self.__class__.genomeName_list[lib_i]
+                if info != None:
+                    if name != genome_basename:
+                        self.__class__.genomeInfo_list[lib_i] = None
+                        self.__class__.genomeName_list[lib_i] = None
+                    else:
+                        return info
+            except:
+                pass
+
+        # 1) transform genbank to kbase genome object and upload to ws
+        shared_dir = "/kb/module/work/tmp"
+        genome_data_file = 'data/genomes/'+genome_basename+'.gbff'
+        genome_file = os.path.join(shared_dir, os.path.basename(genome_data_file))
+        shutil.copy(genome_data_file, genome_file)
+
+        SERVICE_VER = 'release'
+        #SERVICE_VER = 'dev'
+        GFU = GenomeFileUtil(os.environ['SDK_CALLBACK_URL'],
+                             token=self.getContext()['token'],
+                             service_ver=SERVICE_VER
+                         )
+        print ("UPLOADING genome: "+genome_basename+" to WORKSPACE "+self.getWsName()+" ...")
+        genome_upload_result = GFU.genbank_to_genome({'file': {'path': genome_file },
+                                                      'workspace_name': self.getWsName(),
+                                                      'genome_name': genome_basename
+                                                  })
+#                                                  })[0]
+        pprint(genome_upload_result)
+        genome_ref = genome_upload_result['genome_ref']
+        new_obj_info = self.getWsClient().get_object_info_new({'objects': [{'ref': genome_ref}]})[0]
+
+        # 2) store it
+        if not hasattr(self.__class__, 'genomeInfo_list'):
+            self.__class__.genomeInfo_list = []
+            self.__class__.genomeName_list = []
+        for i in range(lib_i+1):
+            try:
+                assigned = self.__class__.genomeInfo_list[i]
+            except:
+                self.__class__.genomeInfo_list.append(None)
+                self.__class__.genomeName_list.append(None)
+
+        self.__class__.genomeInfo_list[lib_i] = new_obj_info
+        self.__class__.genomeName_list[lib_i] = genome_basename
+        return new_obj_info
+
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
+
 
     ### Annotate domains in a GenomeSet
-    def HIDE_annotateDomains_Set(self):
+    def test_run_DomainAnnotation_Sets_01(self):
+        method = 'run_DomainAnnotation_Sets'
 
-        # make a simple GenomeSet that refers to two public Genomes
-        reference_prok_genomes_WS = 'ReferenceDataManager'  # PROD and CI
-        #reference_prok_genomes_WS = '19217'  # PROD
-        #reference_prok_genomes_WS = '15792'  # CI
-        genome_ref_1 = reference_prok_genomes_WS+'/GCF_001566335.1/1'  # E. coli K-12 MG1655
-        genome_ref_2 = reference_prok_genomes_WS+'/GCF_000021385.1/1'  # D. vulgaris str. 'Miyazaki F'        
+        print ("\n\nRUNNING: test_"+method+"_01()")
+        print ("==================================================\n\n")
 
+        # input_data
+        genomeInfo_0 = self.getGenomeInfo('GCF_000287295.1_ASM28729v1_genomic', 0)  # Candidatus Carsonella ruddii HT isolate Thao2000
+        genomeInfo_1 = self.getGenomeInfo('GCF_000306885.1_ASM30688v1_genomic', 1)  # Wolbachia endosymbiont of Onchocerca ochengi
+        genomeInfo_2 = self.getGenomeInfo('GCF_001439985.1_wTPRE_1.0_genomic',  2)  # Wolbachia endosymbiont of Trichogramma pretiosum
+        genomeInfo_3 = self.getGenomeInfo('GCF_000022285.1_ASM2228v1_genomic',  3)  # Wolbachia sp. wRi
+
+        genome_ref_0 = self.getWsName() + '/' + str(genomeInfo_0[0]) + '/' + str(genomeInfo_0[4])
+        genome_ref_1 = self.getWsName() + '/' + str(genomeInfo_1[0]) + '/' + str(genomeInfo_1[4])
+        genome_ref_2 = self.getWsName() + '/' + str(genomeInfo_2[0]) + '/' + str(genomeInfo_2[4])
+        genome_ref_3 = self.getWsName() + '/' + str(genomeInfo_3[0]) + '/' + str(genomeInfo_3[4])
+
+        #feature_id_0 = 'A355_RS00030'   # F0F1 ATP Synthase subunit B
+        #feature_id_1 = 'WOO_RS00195'    # F0 ATP Synthase subunit B
+        #feature_id_2 = 'AOR14_RS04755'  # F0 ATP Synthase subunit B
+        #feature_id_3 = 'WRI_RS01560'    # F0 ATP Synthase subunit B
+
+        genome_ref_list = [genome_ref_0, genome_ref_1]
+        genome_scinames = dict()
+        genome_objnames = dict()
+        genome_refs_by_objname = dict()
+        genome_scinames[genome_ref_0] = 'Candidatus Carsonella ruddii HT isolate Thao2000'
+        genome_scinames[genome_ref_1] = 'Wolbachia endosymbiont of Onchocerca ochengi'
+        for genome_ref in genome_ref_list: 
+            try:
+                [OBJID_I, NAME_I, TYPE_I, SAVE_DATE_I, VERSION_I, SAVED_BY_I, WSID_I, WORKSPACE_I, CHSUM_I, SIZE_I, META_I] = range(11)  # object_info tuple
+                obj_info = self.getWsClient().get_object_info_new ({'objects':[{'ref':genome_ref}]})[0]
+                obj_name = obj_info[NAME_I]
+                genome_objnames[genome_ref] = obj_name
+                genome_refs_by_objname[obj_name] = genome_ref
+            except Exception as e:
+                raise ValueError('Unable to get object from workspace: (' + genome_ref +')' + str(e))
+
+        # build GenomeSet obj
         testGS = {
             'description': 'two genomes',
-            'elements': {
-                'Ecoli_MG1655': {
-                    'ref': genome_ref_1
-                },
-                'Dvulgaris_Miyazaki': {
-                    'ref': genome_ref_2
-                }
-            }
+            'elements': dict()
         }
+        for genome_ref in genome_ref_list: 
+            testGS['elements'][genome_scinames[genome_ref]] = { 'ref': genome_ref }
 
         obj_info = self.getWsClient().save_objects({'workspace': self.getWsName(),       
                                                     'objects': [
@@ -125,3 +212,27 @@ class kb_phylogenomicsTest(unittest.TestCase):
         print('RESULT:')
         pprint(result)
 
+        # check the output DomainAnnotation objects to make sure all domain annotations are done
+        domain_annot_done = dict()
+        for ws_id in self.getWsName():
+            try:
+                dom_annot_obj_info_list = self.getWsClient().list_objects({'ids':[ws_id],'type':"KBaseGeneFamilies.DomainAnnotation"})
+            except Exception as e:
+                raise ValueError ("Unable to list DomainAnnotation objects from workspace: "+str(ws_id)+" "+str(e))
+
+            for info in dom_annot_obj_info_list:
+                [OBJID_I, NAME_I, TYPE_I, SAVE_DATE_I, VERSION_I, SAVED_BY_I, WSID_I, WORKSPACE_I, CHSUM_I, SIZE_I, META_I] = range(11)  # object_info tuple
+                
+                dom_annot_ref = str(info[WSID_I])+'/'+str(info[OBJID_I])+'/'+str(info[VERSION_I])
+                try:
+                    domain_data = self.getWsClient().get_objects2({'objects':[{'ref':dom_annot_ref}]})['data'][0]['data']
+                except:
+                    raise ValueError ("unable to fetch domain annotation: "+dom_annot_ref)
+
+                # read domain data object
+                this_genome_ref = domain_data['genome_ref']
+                if this_genome_ref not in genome_ref_list:
+                    continue
+                domain_annot_done[this_genome_ref] = True
+
+        self.assertEqual(len(domain_annot_done.keys()), genome_ref_list)
