556be4323fdb6ed2ef17f02dce8eb3495a699f03
dcchivian <dcchivian@lbl.gov>
2017-11-04 12:39:39 -0700

    adding view_tree() and trim_tree_to_genomeSet()

---
+++ lib/src/us/kbase/kbphylogenomics/TrimTreeToGenomeSetInput.java
@@ -0,0 +1,100 @@
+
+package us.kbase.kbphylogenomics;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * <p>Original spec-file type: trim_tree_to_genomeSet_Input</p>
+ * <pre>
+ * trim_tree_to_genomeSet()
+ * **
+ * ** trim a KBase Tree to match genomeset, and make newick and images downloadable
+ * </pre>
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("com.googlecode.jsonschema2pojo")
+@JsonPropertyOrder({
+    "workspace_name",
+    "input_genomeSet_ref",
+    "input_tree_ref"
+})
+public class TrimTreeToGenomeSetInput {
+
+    @JsonProperty("workspace_name")
+    private String workspaceName;
+    @JsonProperty("input_genomeSet_ref")
+    private String inputGenomeSetRef;
+    @JsonProperty("input_tree_ref")
+    private String inputTreeRef;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("workspace_name")
+    public String getWorkspaceName() {
+        return workspaceName;
+    }
+
+    @JsonProperty("workspace_name")
+    public void setWorkspaceName(String workspaceName) {
+        this.workspaceName = workspaceName;
+    }
+
+    public TrimTreeToGenomeSetInput withWorkspaceName(String workspaceName) {
+        this.workspaceName = workspaceName;
+        return this;
+    }
+
+    @JsonProperty("input_genomeSet_ref")
+    public String getInputGenomeSetRef() {
+        return inputGenomeSetRef;
+    }
+
+    @JsonProperty("input_genomeSet_ref")
+    public void setInputGenomeSetRef(String inputGenomeSetRef) {
+        this.inputGenomeSetRef = inputGenomeSetRef;
+    }
+
+    public TrimTreeToGenomeSetInput withInputGenomeSetRef(String inputGenomeSetRef) {
+        this.inputGenomeSetRef = inputGenomeSetRef;
+        return this;
+    }
+
+    @JsonProperty("input_tree_ref")
+    public String getInputTreeRef() {
+        return inputTreeRef;
+    }
+
+    @JsonProperty("input_tree_ref")
+    public void setInputTreeRef(String inputTreeRef) {
+        this.inputTreeRef = inputTreeRef;
+    }
+
+    public TrimTreeToGenomeSetInput withInputTreeRef(String inputTreeRef) {
+        this.inputTreeRef = inputTreeRef;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperties(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return ((((((((("TrimTreeToGenomeSetInput"+" [workspaceName=")+ workspaceName)+", inputGenomeSetRef=")+ inputGenomeSetRef)+", inputTreeRef=")+ inputTreeRef)+", additionalProperties=")+ additionalProperties)+"]");
+    }
+
+}
