1e3d49ca98a29e8db97506f236a8f6b27165357d
dcchivian <dcchivian@lbl.gov>
2019-01-11 11:54:44 -0800

    fixed bug of repeating query

--- lib/kb_phylogenomics/kb_phylogenomicsImpl.py
+++ lib/kb_phylogenomics/kb_phylogenomicsImpl.py
@@ -6326,7 +6326,7 @@ This module contains methods for running and visualizing results of phylogenomic
             output_individual_FS_name = input_individual_FS_name+'-'+intree_name+'.BLASTp_homologs'
 
             BLASTp_Params = {'workspace_name':       params['workspace_name'],
-                             'input_one_ref':        individual_FS_ref,
+                             'input_one_ref':        input_individual_FS_ref,
                              'input_many_ref':       tree_derived_genomeSet_ref,
                              'output_filtered_name': output_individual_FS_name,
                              'ident_thresh':         params['ident_thresh'],
@@ -6337,7 +6337,7 @@ This module contains methods for running and visualizing results of phylogenomic
                              }
 
             self.log (console, "running BLASTp for "+input_individual_FS_name)
-            self.log(console, "\n" + pformat(BLASTp_Params))
+            #self.log(console, "\n" + pformat(BLASTp_Params))
 
             blast_retVal = blastClient.BLASTp_Search(BLASTp_Params)
             this_report_name = blast_retVal['report_name']
